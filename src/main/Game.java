22c5d261331e57fb4c11744877215d3fff762199
NguyenPhuc258 <153776093+NguyenPhuc258@users.noreply.github.com>
2024-06-14 17:59:16 +1000

    Mouse and Keyboard inputs events added - GamePanel now controls mouse and keyboard inputs from user. It has new several methods to implement the movement of the rectangle - MouseInputs and KeyboardInputs are added to GamePanel class and store GamePanel to adjust its values

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,5 +6,8 @@ public class Game {
   public Game(){
     gamePanel = new GamePanel();
     gameWindow = new GameWindow(gamePanel);
+
+    // Focus on the events added to the gamePanel such as key
+    gamePanel.requestFocus();
   }
 }
